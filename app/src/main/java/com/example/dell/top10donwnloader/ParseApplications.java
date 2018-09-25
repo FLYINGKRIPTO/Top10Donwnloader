acaf1d991d1f028d8ed2221e1262fc909e4377d8
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-09-25 14:53:32 +0530

    Improved adapter to use less memory and be more efficient using ViewHolders

--- app/src/main/java/com/example/dell/top10donwnloader/ParseApplications.java
+++ app/src/main/java/com/example/dell/top10donwnloader/ParseApplications.java
@@ -19,34 +19,34 @@ public class ParseApplications {
     public ArrayList<FeedEntry> getApplications() {
         return applications;
     }
-    public boolean parse(String xmlData){
+
+    public boolean parse(String xmlData) {
         boolean status = true;
-        FeedEntry currentRecord= null;
+        FeedEntry currentRecord = null;
         boolean inEntry = false;
         boolean gotImage = false;
         String textValue = "";
-        try{
-            XmlPullParserFactory factory =  XmlPullParserFactory.newInstance();
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             factory.setNamespaceAware(true);
             XmlPullParser xpp = factory.newPullParser();
             StringReader stringReader = new StringReader(xmlData);
             xpp.setInput(stringReader);
             int eventType = xpp.getEventType();
-            while(eventType!=XmlPullParser.END_DOCUMENT){
+            while (eventType != XmlPullParser.END_DOCUMENT) {
                 String tagName = xpp.getName();
-                switch (eventType){
+                switch (eventType) {
                     case XmlPullParser.START_TAG:
-                        Log.d(TAG, "parse: Starting Tag for "+ tagName);
-                        if("entry".equalsIgnoreCase(tagName)){
+                        Log.d(TAG, "parse: Starting Tag for " + tagName);
+                        if ("entry".equalsIgnoreCase(tagName)) {
                             inEntry = true;
                             currentRecord = new FeedEntry();
-                        }
-                        else if(("image".equalsIgnoreCase(tagName))&& inEntry){
-                            String imageResolution = xpp.getAttributeValue(null,"height");
-                            if(imageResolution != null){
+                        } else if (("image".equalsIgnoreCase(tagName)) && inEntry) {
+                            String imageResolution = xpp.getAttributeValue(null, "height");
+                            if (imageResolution != null) {
                                 gotImage = "53".equalsIgnoreCase(imageResolution);
                             }
-                    }
+                        }
                         break;
 
                     case XmlPullParser.TEXT:
@@ -54,41 +54,35 @@ public class ParseApplications {
                         break;
 
                     case XmlPullParser.END_TAG:
-                        Log.d(TAG, "parse: Ending Tag for  "+tagName);
-                        if(inEntry){
-                            if("entry".equalsIgnoreCase(tagName)){
+                        Log.d(TAG, "parse: Ending Tag for  " + tagName);
+                        if (inEntry) {
+                            if ("entry".equalsIgnoreCase(tagName)) {
                                 applications.add(currentRecord);
                                 inEntry = false;
-                            }
-                            else if("name".equalsIgnoreCase(tagName)){
+                            } else if ("name".equalsIgnoreCase(tagName)) {
                                 currentRecord.setName(textValue);
-                            }
-                            else if("artist".equalsIgnoreCase(tagName)){
+                            } else if ("artist".equalsIgnoreCase(tagName)) {
                                 currentRecord.setArtist(textValue);
-                            }
-                            else if("releaseDate".equalsIgnoreCase(tagName)){
+                            } else if ("releaseDate".equalsIgnoreCase(tagName)) {
                                 currentRecord.setReleaseDate(textValue);
-                            }
-                            else if("summary".equalsIgnoreCase(tagName)){
+                            } else if ("summary".equalsIgnoreCase(tagName)) {
                                 currentRecord.setSummary(textValue);
-                            }
-                            else if("image".equalsIgnoreCase(tagName)) {
+                            } else if ("image".equalsIgnoreCase(tagName)) {
                                 if (gotImage) {
                                     currentRecord.setImageUrl(textValue);
                                 }
                             }
                         }
                         break;
-                        default:
+                    default:
                 }
                 eventType = xpp.next();
             }
-            for(FeedEntry app:applications){
+            for (FeedEntry app : applications) {
                 Log.d(TAG, "parse: *******************");
-                Log.d(TAG,app.toString());
+                Log.d(TAG, app.toString());
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             status = false;
 
         }
